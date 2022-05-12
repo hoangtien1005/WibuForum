07b35df568c661cad6b4b237e68e654df6078a4f
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-05-12 19:03:12 +0700

    update server and add env file

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,4 @@
 const userModel = require("../models/user.model")
-const favoriteModel = require("../models/favorite.model")
-const postModel = require("../models/post.model")
 const errorModel = require("../models/error.model")
 const { ERROR_CODE } = require("../constants")
 const { formatResponseData } = require("../utils")
@@ -30,91 +28,6 @@ module.exports = {
       if (!data) return res.json(errorModel(404, "User not found."))
 
       return res.json({ data })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-  // [GET] /user/:id/favorites
-  getUserFavorite: async (req, res) => {
-    try {
-      const { id } = req.params
-
-      const { data } = await favoriteModel.getFavoriteByUserId(id)
-
-      if (!data) return res.json(errorModel(404, "User not found."))
-
-      return res.json({ data })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-
-  // [POST] /user/:id/favorites
-  createUserFavorite: async (req, res) => {
-    try {
-      const { id } = req.params
-
-      const { data } = await favoriteModel.create({ ...req.body, user_id: id })
-
-      if (!data) return res.json(errorModel(404, "User not found."))
-
-      return res.json({ data })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-
-  // [DELETE] /user/:id/favorites/:media_id
-  destroyUserFavorite: async (req, res) => {
-    try {
-      const { id, media_id } = req.params
-
-      await favoriteModel.deleteById(id, media_id)
-
-      return res.json({ data: "Deleted successfully" })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-
-  // [GET] /user/:id/posts
-  getUserPost: async (req, res) => {
-    try {
-      const { id } = req.params
-
-      const { data } = await postModel.getPostByUserId(id)
-
-      if (!data) return res.json(errorModel(404, "User not found."))
-
-      return res.json({ data })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-
-  // [POST] /user/:id/posts
-  createUserPost: async (req, res) => {
-    try {
-      const { id } = req.params
-
-      const { data } = await postModel.create({ ...req.body, user_id: id })
-
-      if (!data) return res.json(errorModel(404, "User not found."))
-
-      return res.json({ data })
-    } catch (err) {
-      return res.json(errorModel())
-    }
-  },
-
-  // [DELETE] /user/:id/posts/:media_id
-  destroyUserPost: async (req, res) => {
-    try {
-      const { id, media_id } = req.params
-
-      await postModel.deleteById(id, media_id)
-
-      return res.json({ data: "Deleted successfully" })
     } catch (err) {
       return res.json(errorModel())
     }
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -5,11 +5,5 @@ const userController = require("../controllers/user.controller")
 
 router.get("/", userController.get)
 router.get("/:id", userController.getProfile)
-router.get("/:id/favorites", userController.getUserFavorite)
-router.post("/:id/favorites", userController.createUserFavorite)
-router.delete("/:id/favorites/:media_id", userController.destroyUserFavorite)
-router.get("/:id/posts", userController.getUserPost)
-router.post("/:id/posts", userController.createUserPost)
-router.delete("/:id/posts/:media_id", userController.destroyUserPost)
 
 module.exports = router
